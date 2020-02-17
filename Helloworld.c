31529f9e7e18303b6579a3cd3255430c363a36f4
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-17 15:39:05 +0330

    my first commit!

---
+++ Helloworld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char* remote_str = "Hello World! I'm helloworld";
+    printf("%s", remote_str);
+    getchar();
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char* remote_str = "Hello World!";
+    printf("%s", remote_str);
+    getchar();
+    return 0;
+}
