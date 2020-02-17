7e5885cb0a45cd48a7bd5aecf52eaf66b514b997
mohammad <39569573+mhdan@users.noreply.github.com>
2020-02-17 18:20:28 +0330

    Update Helloworld.c

--- Helloworld.c
+++ Helloworld.c
@@ -2,7 +2,9 @@
 
 int main(){
     char* remote_str = "Hello World! I'm helloworld";
+    char* test_str = "Hello World! I'm test";
     printf("%s", remote_str);
+    printf("%s", test_str);
     getchar();
     return 0;
 }
