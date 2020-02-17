fb5d6cbd11cdba89f3bea4f4ee953f2b5db461fe
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-17 18:28:28 +0330

    second commit for testing stage and understanding

--- Helloworld.c
+++ Helloworld.c
@@ -2,9 +2,7 @@
 
 int main(){
     char* remote_str = "Hello World! I'm helloworld";
-    char* test_str = "Hello World! I'm test";
     printf("%s", remote_str);
-    printf("%s", test_str);
     getchar();
     return 0;
 }
