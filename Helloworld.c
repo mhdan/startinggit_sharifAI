842f5cee901dea53028c673cde352ccaee25abb7
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-18 17:59:48 +0330

    2 lines again add with commit only

--- Helloworld.c
+++ Helloworld.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    char* local_str = "Hello World! I'm helloworld";
+    char* local_str = "Hello World! I'm remote helloworld";
+    char* remote_str = "Hello World! I'm local helloworld";
     printf("%s", local_str);
+    printf("%s", remote_str);
     getchar();
     return 0;
 }
