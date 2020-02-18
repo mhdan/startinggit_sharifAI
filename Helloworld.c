146716f9645bd55d2799448439301d317bba88f0
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-18 19:01:41 +0330

    first in this branch

--- Helloworld.c
+++ Helloworld.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    char* local_str = "Hello World! I'm remote helloworld";
-    char* remote_str = "Hello World! I'm local helloworld";
+    char* local_str = "Hello World! I'm only local!!";
     printf("%s", local_str);
-    printf("%s", remote_str);
     getchar();
     return 0;
 }
