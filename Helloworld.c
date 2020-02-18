4f82a443e545de9f7591a0880f7819cff8084a26
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-18 11:24:19 +0330

    two lines added

--- Helloworld.c
+++ Helloworld.c
@@ -2,7 +2,9 @@
 
 int main(){
     char* remote_str = "Hello World! I'm helloworld";
+    char* local_str = "Hello World! I'm local";
     printf("%s", remote_str);
+    printf("%s", local_str);
     getchar();
     return 0;
 }
