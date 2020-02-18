6855ee074b5726d0b6ac1b02491e9416a45451f9
mzDAN <mh.ziaei.dan@gmail.com>
2020-02-18 11:30:23 +0330

    delete one lines

--- Helloworld.c
+++ Helloworld.c
@@ -2,9 +2,7 @@
 
 int main(){
     char* remote_str = "Hello World! I'm helloworld";
-    char* local_str = "Hello World! I'm local";
     printf("%s", remote_str);
-    printf("%s", local_str);
     getchar();
     return 0;
 }
